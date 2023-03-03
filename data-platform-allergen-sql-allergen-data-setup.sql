5abd4f74e2f119efe47c7b4529c602e6c11d0f69
MiyuYokoyama0109 <miyu.y@latona.io>
2023-03-03 15:42:10 +0900

    release

--- data-platform-allergen-sql-allergen-data-setup.sql
+++ data-platform-allergen-sql-allergen-data-setup.sql
@@ -2,30 +2,30 @@ LOCK TABLES `data_platform_allergen_allergen_data` WRITE;
 
 INSERT INTO `data_platform_allergen_allergen_data` (`Allergen`)
 VALUES
-    ('EGG');
-    ('SOYBEAN');
-    ('MILK');
-    ('WHEAT');
-    ('PEANUT');
-    ('BUCKWHEAT');
-    ('SESAME');
-    ('SHRIMP');
-    ('CRAB');
-    ('ABALONE');
-    ('CUTTLEFISH');
-    ('SALMON_ROW');
-    ('SALMON');
-    ('MACKEREL');
-    ('BEEF');
-    ('PORK');
-    ('CHICKEN');
-    ('GELATIN');
-    ('KIWI_FRUIT');
-    ('YAM');
-    ('WALNUT');
-    ('CASHEWNNUT');
-    ('BANANA');
-    ('APPLE');
-    ('PEACH');
+    ('EGG'),
+    ('SOYBEAN'),
+    ('MILK'),
+    ('WHEAT'),
+    ('PEANUT'),
+    ('BUCKWHEAT'),
+    ('SESAME'),
+    ('SHRIMP'),
+    ('CRAB'),
+    ('ABALONE'),
+    ('CUTTLEFISH'),
+    ('SALMON_ROW'),
+    ('SALMON'),
+    ('MACKEREL'),
+    ('BEEF'),
+    ('PORK'),
+    ('CHICKEN'),
+    ('GELATIN'),
+    ('KIWI_FRUIT'),
+    ('YAM'),
+    ('WALNUT'),
+    ('CASHEWNNUT'),
+    ('BANANA'),
+    ('APPLE'),
+    ('PEACH'),
     ('MATSUTAKE_MUSHROOM');
 UNLOCK TABLES;
